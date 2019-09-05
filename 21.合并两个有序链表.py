062d471ca16edd009a590035a33c04ba1847e609
scratkong <scratkong@163.com>
2019-09-05 21:19:47 +0800

    add 21  merge two single link list

---
+++ 21.合并两个有序链表.py
@@ -0,0 +1,33 @@
+# 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+#
+# 示例：
+#
+# 输入：1->2->4, 1->3->4
+# 输出：1->1->2->3->4->4
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(0)
+        res = head
+
+        while l1 != None and l2 != None:
+            if l1.val > l2.val:
+                head.next = l2
+                l2 = l2.next
+            else:
+                head.next = l1
+                l1 = l1.next
+            head = head.next
+        if l1 == None:
+            head.next = l2
+        elif l2 == None:
+            head.next = l1
+        return res.next
